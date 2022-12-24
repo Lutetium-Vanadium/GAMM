0a1f72c64de0fba36c4082344698a855318d2d80
Lutetium-Vanadium <luv.s7000@gmail.com>
2022-12-24 13:13:50 +0530

    fix typo causing compile fail for self svd parallel BAMM bin

--- src/bin/jts_multi_amm.rs
+++ src/bin/jts_multi_amm.rs
@@ -1,5 +1,5 @@
 fn main() {
     let config = gamm::get_config();
     let (x, y) = gamm::load_matrices(&config).expect("Couldn't load matrices");
-    std::hint::black_box(gamm::self_svd_multi::beta_coocurring_amm(&x, &y, &config));
+    std::hint::black_box(gamm::jts_multi::beta_coocurring_amm(&x, &y, &config));
 }
