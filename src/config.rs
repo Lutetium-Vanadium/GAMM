e4c4e7f378087e82806b4cee4ba300647c16bf7a
Lutetium-Vanadium <luv.s7000@gmail.com>
2023-01-01 18:53:59 +0530

    Update config to use serde(default)

--- src/config.rs
+++ src/config.rs
@@ -7,51 +7,58 @@ use serde::Deserialize;
 
 use crate::common;
 
-#[derive(Default, Deserialize)]
-struct ConfigOptional {
-    x: Option<PathBuf>,
-    y: Option<PathBuf>,
-    l: Option<usize>,
-    beta: Option<f32>,
-    t: Option<usize>,
-    bin: Option<String>,
-}
-
-#[derive(Debug)]
+#[derive(Deserialize, Debug)]
 pub struct Config {
+    #[serde(default = "defaults::x")]
     pub x: PathBuf,
+    #[serde(default = "defaults::y")]
     pub y: PathBuf,
+    #[serde(default = "defaults::l")]
     pub l: usize,
-    pub beta: f32,
+    #[serde(default = "defaults::beta")]
+    pub beta: common::Float,
+    #[serde(default = "common::hardware_concurrency")]
     pub t: usize,
     pub bin: Option<String>,
 }
 
 impl Default for Config {
     fn default() -> Self {
-        Self::from_optional(Default::default())
+        Self {
+            x: defaults::x(),
+            y: defaults::y(),
+            l: defaults::l(),
+            beta: defaults::beta(),
+            t: common::hardware_concurrency(),
+            bin: None,
+        }
     }
 }
 
 impl Config {
-    fn from_optional(optional: ConfigOptional) -> Self {
-        Config {
-            x: optional
-                .x
-                .unwrap_or_else(|| PathBuf::from("./baseline/x.dat")),
-            y: optional
-                .y
-                .unwrap_or_else(|| PathBuf::from("./baseline/y.dat")),
-            l: optional.l.unwrap_or(400),
-            beta: optional.beta.unwrap_or(28.0),
-            t: optional.t.unwrap_or_else(common::hardware_concurrency),
-            bin: optional.bin,
-        }
+    pub fn from_file(file: &Path) -> io::Result<Option<Self>> {
+        Ok(toml::from_str(&std::fs::read_to_string(file)?).ok())
+    }
+}
+
+// Serde defaults take in a function to call for the default, it cannot have default by value
+mod defaults {
+    use crate::common::Float;
+    use std::path::PathBuf;
+
+    pub(super) fn x() -> PathBuf {
+        PathBuf::from("./matrices/x.dat")
     }
 
-    pub fn from_file(file: &Path) -> io::Result<Option<Self>> {
-        Ok(toml::from_str(&std::fs::read_to_string(file)?)
-            .map(Self::from_optional)
-            .ok())
+    pub(super) fn y() -> PathBuf {
+        PathBuf::from("./matrices/y.dat")
+    }
+
+    pub(super) fn l() -> usize {
+        400
+    }
+
+    pub(super) fn beta() -> Float {
+        28.0
     }
 }
